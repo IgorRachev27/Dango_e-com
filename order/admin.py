819048eae4c4139d5c5e950cc034f8dcf1400d0e
IgorRachev27 <iar96@ya.ru>
2022-03-20 00:33:52 +0300

    Youtube final+admin

--- order/admin.py
+++ order/admin.py
@@ -2,4 +2,21 @@ from django.contrib import admin
 from .models import Order
 # Register your models here.
 
-admin.site.register(Order)
+
+
+
+class OrderAdm(admin.ModelAdmin):
+    list_display = ('id','user', 'product','quantity','phone','date')
+    list_display_links = ('id','product')
+    search_fields = ('id','user', 'product','date')
+    list_filter = ('date',) # запятая обязательна чтобы пайтон увидел строку как кортеж
+    list_editable = ('quantity', 'phone')
+    list_per_page = 10
+    list_max_show_all = 100
+    fields = ('id','user', 'product','quantity','phone','date')
+    readonly_fields = ('date', 'id', 'user', 'product')
+    # поле класса coment
+    # inlines = [Comment,]
+
+
+admin.site.register(Order,OrderAdm)
